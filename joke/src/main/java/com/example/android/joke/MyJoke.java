d211e6882ff8d8e9a6af502655811ffb2b80bdf6
fireflyfif <fif.iva@gmail.com>
2018-06-14 13:12:27 +0200

    add jokes to an array and select a random joke each time

--- joke/src/main/java/com/example/android/joke/MyJoke.java
+++ joke/src/main/java/com/example/android/joke/MyJoke.java
@@ -1,8 +1,25 @@
 package com.example.android.joke;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class MyJoke {
 
     public String getJoke() {
-        return "Here is another joke.";
+
+        String randomJoke;
+        Random randomNumber = new Random();
+
+        List<String> jokeList = new ArrayList<>();
+        jokeList.add("first joke");
+        jokeList.add("second joke");
+        jokeList.add("third joke");
+        jokeList.add("fourth joke");
+        jokeList.add("fifth joke");
+
+        randomJoke = jokeList.get(randomNumber.nextInt(jokeList.size()));
+
+        return randomJoke;
     }
 }
