3105f0dfddef02016629f114a4e879460207d342
fireflyfif <fif.iva@gmail.com>
2018-06-20 13:48:04 +0200

    fix issue with getting the local IP Address of the machine, moved to buildConfig, gradle

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.android.joker.JokeLibraryActivity;
@@ -18,20 +19,26 @@ public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
     public static final String JOKE_KEY = "joke";
 
+    private Button mJokeGivingButton;
+    private ProgressBar mLoadingIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button jokeGivingButton = findViewById(R.id.joke_me_button);
-        jokeGivingButton.setOnClickListener(new View.OnClickListener() {
+        mJokeGivingButton = findViewById(R.id.joke_me_button);
+        mLoadingIndicator = findViewById(R.id.progress_bar);
+
+        mLoadingIndicator.setVisibility(View.GONE);
+
+        mJokeGivingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                mLoadingIndicator.setVisibility(View.VISIBLE);
                 tellJoke();
             }
         });
-
     }
 
     @Override
@@ -58,10 +65,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void tellJoke() {
 
+        // Hide the Progress Bar when the app is trying to retrieve the joke from the backend
+        mLoadingIndicator.setVisibility(View.GONE);
+
         JokerAsyncTask jokerAsyncTask = new JokerAsyncTask(getApplicationContext(),
                 new JokerAsyncTask.OnEventListener<String>() {
                     @Override
                     public void onSuccess(String joke) {
+
                         Intent intent = new Intent(getApplicationContext(), JokeLibraryActivity.class);
                         intent.putExtra(JOKE_KEY, joke);
                         startActivity(intent);
--- app/src/main/java/com/udacity/gradle/builditbigger/utils/GetIpAddress.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/utils/GetIpAddress.java
@@ -4,6 +4,9 @@ import android.os.Build;
 import android.text.format.Formatter;
 import android.util.Log;
 
+import com.udacity.gradle.builditbigger.BuildConfig;
+
+import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
@@ -14,12 +17,15 @@ public class GetIpAddress {
     private static final String LOG_TAG = GetIpAddress.class.getSimpleName();
 
     // - 10.0.2.2 is localhost's IP address in Android emulator
-    private static final String EMULATOR_IP_ADDRESS = "http://10.0.2.2:8080/_ah/api/";
+    private static final String EMULATOR_IP_ADDRESS = "10.0.2.2";
+    private static final String LOCAL_SERVER_IP = "http://" + EMULATOR_IP_ADDRESS + ":8080/_ah/api/";
+
+
 
     /**
      * Method for checking if the tested device is an emulator
-     *
      * recourse: https://stackoverflow.com/a/21505193/8132331
+     *
      * @return boolean whether the build is being performed on an Emulator
      */
     public static boolean checkIsEmulator() {
@@ -34,36 +40,51 @@ public class GetIpAddress {
     }
 
 
+    /**
+     * Method for getting the IP Address on the testing device whether emulator or real device
+     *
+     * @return String with the IP Address
+     */
     public static String getIpAddress() {
 
         boolean isEmulator = checkIsEmulator();
         Log.d(LOG_TAG, "isEmulator= " + isEmulator);
 
         if (isEmulator) {
-            return EMULATOR_IP_ADDRESS;
+            return LOCAL_SERVER_IP;
         } else {
-            return getLocalIpAddress();
+            // Get the Local IP Address of the machine for testing the backend server on a real devices
+            // resource: http://disq.us/p/1dqd3fz
+            Log.d(LOG_TAG, "Local machine IP Address: " + BuildConfig.LOCAL_API);
+            return BuildConfig.LOCAL_API;
         }
     }
 
     /**
-     * Method for getting the local IP Address of the user
-     *
+     * Method for getting the local IP Address of the device
+     * <p>
+     * TODO: Need to extract IP Address from the computer, not the device
+     * <p>
      * resource: https://stackoverflow.com/a/10199498/8132331
+     *
      * @return String with the IP address
      */
     private static String getLocalIpAddress() {
         try {
             for (Enumeration<NetworkInterface> enumeration = NetworkInterface.getNetworkInterfaces();
-                 enumeration.hasMoreElements();) {
+                 enumeration.hasMoreElements(); ) {
 
                 NetworkInterface networkInterface = enumeration.nextElement();
                 for (Enumeration<InetAddress> enumIpAddress = networkInterface.getInetAddresses();
-                     enumIpAddress.hasMoreElements();) {
+                     enumIpAddress.hasMoreElements(); ) {
 
                     InetAddress inetAddress = enumIpAddress.nextElement();
-                    if (!inetAddress.isLoopbackAddress()) {
-                        String ip = Formatter.formatIpAddress(inetAddress.hashCode());
+                    if (!inetAddress.isLoopbackAddress() && inetAddress instanceof Inet4Address) {
+
+                        // TODO: Extract the computer's IP Address
+                        // This gets the IP of the device,
+                        // but we need the IP Address of the computer!!!
+                        String ip = inetAddress.getHostAddress();
 
                         Log.i(LOG_TAG, "IP address = " + ip);
                         return ip;
